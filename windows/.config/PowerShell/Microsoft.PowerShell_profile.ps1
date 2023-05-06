5bb547e0413cb21eff0d4c46f935e8a97938cb5f
JBillson <justin.billson@gmail.com>
2023-05-06 14:50:29 +0200

    Updated PowerShell profile

--- windows/.config/PowerShell/Microsoft.PowerShell_profile.ps1
+++ windows/.config/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,7 +1,7 @@
 # STYLING
 
-# oh-my-posh
-oh-my-posh init pwsh --config "$HOME/.mytheme.omp.json" | Invoke-Expression
+# [oh my posh]
+oh-my-posh init pwsh --config "$HOME/.config/oh-my-posh/.mytheme.omp.json" | Invoke-Expression
 
 #------------------------------------------------------------------#
 # ALIASES
@@ -11,7 +11,12 @@ function personal {cd "C:\Users\justi\Documents\work\personal\"}
 function eden {cd "C:\Users\justi\Documents\work\eden\"}
 function experiences {cd "C:\Users\justi\Documents\work\eden\experiences"}
 function startup {cd "C:\Users\justi\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"}
+function nvim-config {cd "C:\Users\justi\AppData\Local\nvim"}
 
 # commands
 Set-Alias vim nvim
 
+#------------------------------------------------------------------#
+# ENVIRONMENT VARIABLES
+# [Komorebi]
+$Env:KOMOREBI_CONFIG_HOME = 'C:\Users\justi\.config\komorebi'
