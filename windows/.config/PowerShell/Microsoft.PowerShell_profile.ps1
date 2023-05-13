c594d4ce670c3d1572d7dabe15ec8fc0cf566fd8
JBillson <justin.billson@gmail.com>
2023-05-13 10:49:42 +0200

    updated PowerShell profile

--- windows/.config/PowerShell/Microsoft.PowerShell_profile.ps1
+++ windows/.config/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,27 +1,40 @@
 # STYLING
 
-# [oh my posh]
+# [oh my posh] - init pwsh with oh-my-posh
 oh-my-posh init pwsh --config "$HOME/.config/oh-my-posh/.mytheme.omp.json" | Invoke-Expression
+
+# [terminal-icons]
+Import-Module -Name Terminal-Icons
 
 #------------------------------------------------------------------#
 # ALIASES
 
 # paths
-function personal {cd "C:\Users\justi\Documents\work\personal\"}
-function eden {cd "C:\Users\justi\Documents\work\eden\"}
-function experiences {cd "C:\Users\justi\Documents\work\eden\experiences"}
-function startup {cd "C:\Users\justi\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"}
-function nvim-config {cd "C:\Users\justi\AppData\Local\nvim"}
+function Personal {Set-Location "$HOME\Documents\work\personal\"}
+function Eden {Set-Location "$HOME\Documents\work\eden\"}
+function Experiences {Set-Location "$HOME\Documents\work\eden\experiences"}
+function Startup {Set-Location "$HOME\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"}
+function NvimConfig {Set-Location "$HOME\AppData\Local\nvim"}
+function VimConfig {Set-Location "$HOME\AppData\Local\nvim"}
+
+# commands
+Set-Alias vim nvim
 
 # functions
 function cguid {$guid = New-Guid 
                 $guid | Set-Clipboard
                 Write-Output $guid}
 
-# commands
-Set-Alias vim nvim
-
 #------------------------------------------------------------------#
 # ENVIRONMENT VARIABLES
+
 # [Komorebi]
 $Env:KOMOREBI_CONFIG_HOME = "$HOME\.config\komorebi"
+
+#------------------------------------------------------------------#
+
+# [komorebi] - launch komorebi if not runnning
+$komorebi = Get-Process komorebi -ErrorAction SilentlyContinue
+if (!$komorebi){
+  komorebic start -a
+}
