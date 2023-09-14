ce6abb0887298ec75ce48641b462c74cbfe6826e
JBillson <justin.billson@gmail.com>
2023-09-14 18:28:24 +0200

    removed gaps and active border

--- windows/komorebi/komorebi.ps1
+++ windows/komorebi/komorebi.ps1
@@ -26,44 +26,44 @@ komorebic ensure-named-workspaces 1 a b c d e f g h i
 # komorebic named-workspace-layout I bsp
 
 # Set the gaps around the edge of the screen for a workspace
-komorebic named-workspace-padding I 25 
-komorebic named-workspace-padding II 25 
-komorebic named-workspace-padding III 25 
-komorebic named-workspace-padding IV 25 
-komorebic named-workspace-padding V 25 
-komorebic named-workspace-padding VI 25 
-komorebic named-workspace-padding VII 25 
-komorebic named-workspace-padding VII 25 
-komorebic named-workspace-padding IX 25 
-komorebic named-workspace-padding a 25 
-komorebic named-workspace-padding b 25 
-komorebic named-workspace-padding c 25 
-komorebic named-workspace-padding d 25 
-komorebic named-workspace-padding e 25 
-komorebic named-workspace-padding f 25 
-komorebic named-workspace-padding g 25 
-komorebic named-workspace-padding h 25 
-komorebic named-workspace-padding i 25 
+komorebic named-workspace-padding I 0 
+komorebic named-workspace-padding II 0 
+komorebic named-workspace-padding III 0 
+komorebic named-workspace-padding IV 0 
+komorebic named-workspace-padding V 0 
+komorebic named-workspace-padding VI 0 
+komorebic named-workspace-padding VII 0 
+komorebic named-workspace-padding VII 0 
+komorebic named-workspace-padding IX 0 
+komorebic named-workspace-padding a 0 
+komorebic named-workspace-padding b 0 
+komorebic named-workspace-padding c 0 
+komorebic named-workspace-padding d 0 
+komorebic named-workspace-padding e 0 
+komorebic named-workspace-padding f 0 
+komorebic named-workspace-padding g 0 
+komorebic named-workspace-padding h 0 
+komorebic named-workspace-padding i 0 
 
 # Set the gaps between the containers for a workspace
-komorebic named-workspace-container-padding I 25
-komorebic named-workspace-container-padding II 25
-komorebic named-workspace-container-padding III 25
-komorebic named-workspace-container-padding IV 25
-komorebic named-workspace-container-padding V 25
-komorebic named-workspace-container-padding VI 25
-komorebic named-workspace-container-padding VII 25
-komorebic named-workspace-container-padding VIII 25
-komorebic named-workspace-container-padding IX 25
-komorebic named-workspace-container-padding a 25
-komorebic named-workspace-container-padding b 25
-komorebic named-workspace-container-padding c 25
-komorebic named-workspace-container-padding d 25
-komorebic named-workspace-container-padding e 25
-komorebic named-workspace-container-padding f 25
-komorebic named-workspace-container-padding g 25
-komorebic named-workspace-container-padding h 25
-komorebic named-workspace-container-padding i 25
+komorebic named-workspace-container-padding I 0
+komorebic named-workspace-container-padding II 0
+komorebic named-workspace-container-padding III 0
+komorebic named-workspace-container-padding IV 0
+komorebic named-workspace-container-padding V 0
+komorebic named-workspace-container-padding VI 0
+komorebic named-workspace-container-padding VII 0
+komorebic named-workspace-container-padding VIII 0
+komorebic named-workspace-container-padding IX 0
+komorebic named-workspace-container-padding a 0
+komorebic named-workspace-container-padding b 0
+komorebic named-workspace-container-padding c 0
+komorebic named-workspace-container-padding d 0
+komorebic named-workspace-container-padding e 0
+komorebic named-workspace-container-padding f 0
+komorebic named-workspace-container-padding g 0
+komorebic named-workspace-container-padding h 0
+komorebic named-workspace-container-padding i 0
 
 # You can assign specific apps to named workspaces
 komorebic initial-named-workspace-rule title "YouTube Music" IX 
@@ -72,11 +72,11 @@ komorebic initial-named-workspace-rule title "YouTube Music" IX
 komorebic invisible-borders 7 0 14 7
 
 # Set active window border
-komorebic active-window-border-width 15 
-komorebic active-window-border-offset -- -3 
+komorebic active-window-border-width 5 
+#komorebic active-window-border-offset -- -3 
 komorebic active-window-border-colour 43 122 216 --window-kind single
 komorebic active-window-border-colour 0 255 0 --window-kind stack
 komorebic active-window-border-colour 255 0 0 --window-kind monocle 
-komorebic active-window-border enable 
+komorebic active-window-border disable 
 
 komorebic complete-configuration
