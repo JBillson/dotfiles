f6e169d7c41e42386bd4f0fda2f0fe0355286237
JBillson <justin.billson@gmail.com>
2023-12-19 16:14:07 +0200

    add(nvim): keymap for navigating panes

--- windows/nvim/lua/mappings.lua
+++ windows/nvim/lua/mappings.lua
@@ -4,3 +4,8 @@ vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.g.mapleader = " "
 
+vim.keymap.set('n', '<c-k>', ':wincmd k<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<c-j>', ':wincmd j<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<c-h>', ':wincmd h<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<c-l>', ':wincmd l<CR>', { noremap = true, silent = true })
+
