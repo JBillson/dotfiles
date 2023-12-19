53e41b316c534e6d785d421ce48bbf3a5e6a07e1
JBillson <justin.billson@gmail.com>
2023-12-19 14:03:18 +0200

    - feat(install): check script is running as administrator - add(install): WinDirStat to install list

--- windows/install-apps.ps1
+++ windows/install-apps.ps1
@@ -28,7 +28,8 @@ $install=
 "LGUG2Z.whkd",
 "LGUG2Z.komorebi",
 "BurntSushi.ripgrep.MSVC",
-"sharkdp.fd"
+"sharkdp.fd",
+"WinDirStat.WinDirStat"
 
 $uninstall =
 "Microsoft.OneDrive",
@@ -46,13 +47,20 @@ $uninstall =
 "Microsoft.WindowsMaps_8wekyb3d8bbwe",
 "microsoft.windowscommunicationsapps_8wekyb3d8bbwe"
 
+# Ensure script is running as Administrator
+$currentPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
+  if (!$currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)){
+    Write-Output "Please run this script as Administrator!"
+      Break
+  }
+
 Write-Output "----------------Installing Apps------------------------" 
 foreach ($app in $install){
-    Write-Output "Installing $app" 
+    Write-Output "Installing $app"
     winget install $app -h --no-upgrade
 
     if ($app -eq "LGUG2Z.komorebi"){
-        Write-Output "Komorebi: Enabling Long Path Support" 
+        Write-Output "Komorebi: Enabling Long Path Support"
         Set-ItemProperty 'HKLM:\SYSTEM\CurrentControlSet\Control\FileSystem' -Name 'LongPathsEnabled' -Value 1
     }
 }
@@ -60,7 +68,7 @@ Write-Output "-------------------------------------------------------"
 
 Write-Output "----------------Uninstalling Bloatware-----------------" 
 foreach ($app in $uninstall){
-    Write-Output "Uninstalling $app" 
+    Write-Output "Uninstalling $app"
     winget uninstall $app
 }
 Write-Output "-------------------------------------------------------" 
