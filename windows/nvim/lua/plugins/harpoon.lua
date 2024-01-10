3383a18397d11c34820a194c6c8b837a1f57f1bd
JBillson <justin.billson@gmail.com>
2024-01-10 02:50:45 +0200

    add[nvim]: harpoon and transparent background

---
+++ windows/nvim/lua/plugins/harpoon.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    'nvim-lua/plenary.nvim'
+  },
+  {
+    'ThePrimeagen/harpoon',
+    config = function()
+      local mark = require("harpoon.mark")
+      local ui = require("harpoon.ui")
+      vim.keymap.set('n', '<leader>a', mark.add_file, {})
+      vim.keymap.set('n', '<C-e>', ui.toggle_quick_menu, {})
+      vim.keymap.set('n', '<C-n>', ui.nav_next, {})
+      vim.keymap.set('n', '<C-p>', ui.nav_prev, {})
+    end
+  }
+}
+
