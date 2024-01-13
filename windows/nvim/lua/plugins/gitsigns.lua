92bed1a43bd093f74f3579748b5fe96ea34ad7db
JBillson <justin.billson@gmail.com>
2024-01-14 00:59:09 +0200

    add[nvim]: autopairs plugin

--- windows/nvim/lua/plugins/gitsigns.lua
+++ windows/nvim/lua/plugins/gitsigns.lua
@@ -1,28 +1,28 @@
 return {
-  "lewis6991/gitsigns.nvim",
-  config = function()
-    require("gitsigns").setup()
+	"lewis6991/gitsigns.nvim",
+	config = function()
+		require("gitsigns").setup()
 
-    local gs = package.loaded.gitsigns
-    vim.keymap.set("n", "<leader>hd", gs.diffthis, {})
-    vim.keymap.set("n", "<leader>hp", gs.preview_hunk, {})
-    vim.keymap.set("n", "<leader>hb", gs.toggle_current_line_blame, {})
+		local gs = package.loaded.gitsigns
+		vim.keymap.set("n", "<leader>hd", gs.diffthis, {})
+		vim.keymap.set("n", "<leader>hp", gs.preview_hunk, {})
+		vim.keymap.set("n", "<leader>tb", gs.toggle_current_line_blame, {})
 
-    vim.keymap.set("n", "<leader>hs", gs.stage_hunk, {})
-    vim.keymap.set("n", "<leader>hu", gs.undo_stage_hunk, {})
-    vim.keymap.set("n", "<leader>hr", gs.reset_hunk, {})
-    vim.keymap.set("v", "<leader>hs", function()
-      gs.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
-    end)
-    vim.keymap.set("v", "<leader>hr", function()
-      gs.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
-    end)
+		vim.keymap.set("n", "<leader>hs", gs.stage_hunk, {})
+		vim.keymap.set("n", "<leader>hu", gs.undo_stage_hunk, {})
+		vim.keymap.set("n", "<leader>hr", gs.reset_hunk, {})
+		vim.keymap.set("v", "<leader>hs", function()
+			gs.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
+		end)
+		vim.keymap.set("v", "<leader>hr", function()
+			gs.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
+		end)
 
-    vim.keymap.set("n", "<leader>hS", gs.stage_buffer, {})
+		vim.keymap.set("n", "<leader>hS", gs.stage_buffer, {})
 
-    vim.keymap.set("n", "<leader>hR", gs.reset_buffer, {})
-    vim.keymap.set("n", "<leader>hb", function()
-      gs.blame_line({ full = true })
-    end)
-  end,
+		vim.keymap.set("n", "<leader>hR", gs.reset_buffer, {})
+		vim.keymap.set("n", "<leader>hb", function()
+			gs.blame_line({ full = true })
+		end)
+	end,
 }
