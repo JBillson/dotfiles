81b93585942b0b1451783fd5fea0113a0170361b
JBillson <justin.billson@gmail.com>
2024-01-31 23:49:49 +0200

    fix(install): path error

--- windows/install-scripts/src/dependencies.ps1
+++ windows/install-scripts/src/dependencies.ps1
@@ -4,7 +4,7 @@ scoop bucket add extras
 Write-Output "-------------------------------------------------------" 
 
 Write-Output "-------------Installing Dependencies--------------------" 
-$dependencies = Get-Content ../data/dependencies.txt
+$dependencies = Get-Content ./data/dependencies.txt
 foreach ($app in $dependencies){
     Write-Output "[$app]"
     winget install $app -h --no-upgrade
